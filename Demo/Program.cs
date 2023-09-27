462bee93ca9693dc0bc18fefc4c0bbfdb21533b2
Tom van Dinther <39470469+tvandinther@users.noreply.github.com>
2023-09-27 16:44:29 +0200

    WIP

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -2,3 +2,5 @@
 
 var cls = new Class1();
 Console.WriteLine(cls.Name);
+Console.WriteLine("64-bit: {0}", Environment.Is64BitProcess ? "Yes" : "No");
+cls.PrintViaRust("Hello from C#!");
---
+++ rust-library/build.rs
@@ -0,0 +1,10 @@
+﻿extern crate csbindgen;
+
+fn main() {
+    println!("cargo:warning=build.rs is running!");
+    csbindgen::Builder::default()
+        .input_extern_file("src/client.rs")
+        .csharp_dll_name("libclient_lib.so")
+        .generate_csharp_file("bindings/NativeMethods.g.cs")
+        .unwrap();
+}
