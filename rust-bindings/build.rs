81bf9f72849c0ab1c868185029fb202ae8a43a37
Tom van Dinther <39470469+tvandinther@users.noreply.github.com>
2023-10-13 17:47:52 +0200

    add c# binding Cargo.toml and build script

---
+++ rust-bindings/build.rs
@@ -0,0 +1,13 @@
+﻿extern crate csbindgen;
+
+fn main() {
+    println!("cargo:rerun-if-changed=NULL");
+    csbindgen::Builder::default()
+        .input_extern_file("libsql/crates/bindings/c/src/lib.rs")
+        .input_extern_file("libsql/crates/bindings/c/src/types.rs")
+        .csharp_dll_name("libclient_lib.so")
+        .csharp_namespace("Bindings")
+        .csharp_class_name("Libsql")
+        .generate_csharp_file("bindings/Libsql.g.cs")
+        .unwrap();
+}
