5a89955fef89b7427cceecf19c65a2b154cc8b05
Tom van Dinther <39470469+tvandinther@users.noreply.github.com>
2023-10-16 10:42:27 +0200

    patches to make the compilation work

--- rust-bindings/csharp-bindings/build.rs
+++ rust-bindings/csharp-bindings/build.rs
@@ -5,7 +5,7 @@ fn main() {
     csbindgen::Builder::default()
         .input_extern_file("../libsql/crates/bindings/c/src/lib.rs")
         .input_extern_file("../libsql/crates/bindings/c/src/types.rs")
-        .csharp_dll_name("libclient_lib.so")
+        .csharp_dll_name("libcsharp_bindings.so")
         .csharp_namespace("Bindings")
         .csharp_class_name("Libsql")
         .generate_csharp_file("bindings/Libsql.g.cs")
