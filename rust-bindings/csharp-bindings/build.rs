d42ed17de90b1154846bde0ed79ebda87c70e3ff
Tom van Dinther <39470469+tvandinther@users.noreply.github.com>
2023-10-23 22:27:55 +0200

    Add support for Windows and MacOS runtimes (#7)
    * test new pipeline
    
    * fix branch filter typo
    
    * rearrange pipeline
    
    * Add runner os to cache key
    
    * Remove dotnet setup from dependency build job
    
    * Extract build-dependencies into callable workflow
    
    * add needs
    
    * use bash shell for generate bindings
    
    * Try a windows only build
    
    * print output after
    
    * additional debugging
    
    * remove if
    
    * bash shell
    
    * continue on error
    
    * Hopefully fixed
    
    * change parameters
    
    * fix cache restore
    
    * add macos and bypass
    
    * remove debugging step
    
    * set multipaltform build back to true

--- rust-bindings/csharp-bindings/build.rs
+++ rust-bindings/csharp-bindings/build.rs
@@ -5,7 +5,7 @@ fn main() {
     csbindgen::Builder::default()
         .input_extern_file("../libsql/bindings/c/src/lib.rs")
         .input_extern_file("../libsql/bindings/c/src/types.rs")
-        .csharp_dll_name("libcsharp_bindings.so")
+        .csharp_dll_name("csharp_bindings")
         .csharp_namespace("Libsql")
         .csharp_class_name("Bindings")
         .generate_csharp_file("bindings/Libsql.g.cs")
