39aadf3dd710d4770bdc31fa10c29e2323468ccc
Tom van Dinther <39470469+tvandinther@users.noreply.github.com>
2024-02-19 22:52:02 +0100

    Refactor prepared statement into new class

--- Libsql.Client/DatabaseWrapper.cs
+++ Libsql.Client/DatabaseWrapper.cs
@@ -137,98 +137,29 @@ namespace Libsql.Client
         {
             return await Task.Run(() =>
             {
-                unsafe
-                {
-                    var error = new Error();
-                    int exitCode;
-            
-                    fixed (byte* sqlPtr = Encoding.UTF8.GetBytes(sql))
-                    {
-                        var statement = new libsql_stmt_t();
-                        exitCode = Bindings.libsql_prepare(sqlPtr, &statement, &error.Ptr);
-                        error.ThrowIfNonZero(exitCode, $"Failed to prepare statement for query: {sql}");
-                        return ExecuteStatement(statement);
-                    }
-                }
+                var statement = new Statement(sql);
+                return ExecuteStatement(statement);
             });
         }
 
         public async Task<IResultSet> Execute(string sql, params object[] args)
         {
             return await Task.Run(() => {
-                unsafe {
-                    var statement = new libsql_stmt_t();
-                    var error = new Error();
-                    int exitCode;
-
-                    fixed (byte* sqlPtr = Encoding.UTF8.GetBytes(sql))
-                    {
-                        exitCode = Bindings.libsql_prepare(sqlPtr, &statement, &error.Ptr);
-                    }
-
-                    error.ThrowIfNonZero(exitCode, $"Failed to prepare statement for query: {sql}");
-
-                    if (args is null)
-                    {
-                        exitCode = Bindings.libsql_bind_null(statement, 1, &error.Ptr);
-                        error.ThrowIfNonZero(exitCode, "Failed to bind null parameter");
-                    }
-                    else {
-                        for (var i = 0; i < args.Length; i++)
-                        {
-                            var arg = args[i];
-
-                            if (arg is null)
-                            {
-                                exitCode = Bindings.libsql_bind_null(statement, i + 1, &error.Ptr);
-                                error.ThrowIfNonZero(exitCode, "Failed to bind null parameter");
-                                continue;
-                            }
-
-                            switch (arg)
-                            {
-                                case int val:
-                                    exitCode = Bindings.libsql_bind_int(statement, i + 1, val, &error.Ptr);
-                                    break;
-                                case double d:
-                                    exitCode = Bindings.libsql_bind_float(statement, i + 1, d, &error.Ptr);
-                                    break;
-                                case string s:
-                                    fixed (byte* sPtr = Encoding.UTF8.GetBytes(s))
-                                    {
-                                        exitCode = Bindings.libsql_bind_string(statement, i + 1, sPtr, &error.Ptr);
-                                    }
-                                    break;
-                                case byte[] b:
-                                    fixed (byte* bPtr = b)
-                                    {
-                                        exitCode = Bindings.libsql_bind_blob(statement, i + 1, bPtr, b.Length, &error.Ptr);
-                                    }
-                                    break;
-                                case null:
-                                    exitCode = Bindings.libsql_bind_null(statement, i + 1, &error.Ptr);
-                                    break;
-                                default:
-                                    throw new ArgumentException($"Unsupported argument type: {arg.GetType()}");
-                            }
-                            
-                            error.ThrowIfNonZero(exitCode, $"Failed to bind parameter. Type: {arg.GetType()} Value: {arg}");
-                        }
-                    }
-                    
-                    return ExecuteStatement(statement);
-                }
+                var statement = new Statement(sql);
+                statement.Bind(args);
+                
+                return ExecuteStatement(statement);
             });
         }
 
-        private unsafe IResultSet ExecuteStatement(libsql_stmt_t statement)
+        private unsafe IResultSet ExecuteStatement(Statement statement)
         {
             var error = new Error();
             var rows = new libsql_rows_t();
             int exitCode;
         
-            exitCode = Bindings.libsql_execute_stmt(_connection, statement, &rows, &error.Ptr);
-            Bindings.libsql_free_stmt(statement);
+            exitCode = Bindings.libsql_execute_stmt(_connection, statement.Stmt, &rows, &error.Ptr);
+            statement.Dispose();
 
             error.ThrowIfNonZero(exitCode, "Failed to execute statement");
         
